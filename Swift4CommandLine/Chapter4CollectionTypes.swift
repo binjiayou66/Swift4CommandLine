0dd817e829dfc27facfed24e03bab7067c09934a
naibin.liu <naibin.liu@witown.cn>
2017-06-12 13:21:12 +0800

    Working Place

--- Swift4CommandLine/Chapter4CollectionTypes.swift
+++ Swift4CommandLine/Chapter4CollectionTypes.swift
@@ -90,6 +90,6 @@ class Chapter4CollectionTypes: NSObject {
         }
         
         // Sets
-        
+        // A set stores distinct values of the same type in a collection with no defined ordering. You can use a set instead of an array when the order of items is not important, or when you need to ensure that an item only appears once.
     }
 }
