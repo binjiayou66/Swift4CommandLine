5db59e39e84c0b83a396710e5029cd9f593b3567
naibin <binjiayou66@163.com>
2017-06-19 21:47:09 +0800

    Over

--- Swift4CommandLine/Chapter16ARC.swift
+++ Swift4CommandLine/Chapter16ARC.swift
@@ -18,6 +18,13 @@ class Chapter16ARC: NSObject {
         // (5) Strong Reference Cycles for Closures
         // (6) Resolving Strong Reference Cycles for Closures
         
+        // Automatic Reference Counting
+        // (1) How ARC Works
+        // (2) ARC in Action
+        // (3) Strong Reference Cycles Between Class Instances
+        // (4) Resolving Strong Reference Cycles Between Class Instances
+        // (5) Strong Reference Cycles for Closures
+        // (6) Resolving Strong Reference Cycles for Closures
         
     }
 }
