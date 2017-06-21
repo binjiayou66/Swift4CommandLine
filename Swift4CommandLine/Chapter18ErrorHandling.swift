ac26716b2df0dec505e7d9c0d9b1c685b8ff7003
naibin.liu <naibin.liu@witown.cn>
2017-06-21 18:31:32 +0800

    Chapter 18 and Chapter 19

--- Swift4CommandLine/Chapter18ErrorHandling.swift
+++ Swift4CommandLine/Chapter18ErrorHandling.swift
@@ -15,5 +15,96 @@ class Chapter18ErrorHandling: NSObject {
         // (2) Handling Errors
         // (3) Specifying Cleanup Actions
         
+        // Error Handling
+        // Error handling is the process of responding to and recovering from error conditions in your program. Swift provides first-class support for throwing, catching, propagating, and manipulating recoverable errors at runtime.
+        // Some operations aren’t guaranteed to always complete execution or produce a useful output. Optionals are used to represent the absence of a value, but when an operation fails, it’s often useful to understand what caused the failure, so that your code can respond accordingly.
+        // As an example, consider the task of reading and processing data from a file on disk. There are a number of ways this task can fail, including the <<file not existing>> at the specified path, the file <<not having read permissions>>, or the file <<not being encoded in a compatible format>>. Distinguishing among these different situations allows a program to resolve some errors and to communicate to the user any errors it can’t resolve.
+        
+        // (1) Representing and Throwing Errors
+        // In Swift, errors are represented by values of types that conform to the Error protocol. This empty protocol indicates that a type can be used for error handling.
+        /*
+         enum SomeError: Error {
+             case ErrorA
+             case ErrorB
+         }
+         */
+        // Throwing an error lets you indicate that something unexpected happened and the normal flow of execution can’t continue. You use a throw statement to throw an error.
+        /*
+         throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+         */
+        
+        // (2) Handling Errors
+        // >>1 do try catch
+        do {
+            try self.canThrowErrors()
+        }catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+            print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+        }catch VendingMachineError.invalidSelection {
+            print("Invalid Selection.")
+        }catch VendingMachineError.outOfStock {
+            print("Out of Stock.")
+        }catch {
+            
+        }
+        
+        do {
+            try self.canThrowErrors()
+        }catch {
+            switch error {
+            case VendingMachineError.insufficientFunds(let coinsNeeded):
+                print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+            case VendingMachineError.invalidSelection:
+                print("Invalid Selection.")
+            case VendingMachineError.outOfStock:
+                print("Out of Stock.")
+            default:
+                print("Other Error.")
+            }
+        }
+        // >>2 Converting Errors to Optional Values
+        // result is type of "Int?"
+        let result = try? self.anotherThrowsFunc()
+        print("result = \(result ?? -99)")
+        
+        let sameAsResult: Int?
+        do {
+            sameAsResult = try anotherThrowsFunc()
+        }catch {
+            sameAsResult = nil
+        }
+        print("sameAsResult = \(sameAsResult ?? -99)")
+        
+        // >>3 Disabling Error Propagation
+        // Sometimes you know a throwing function or method won’t, in fact, throw an error at runtime. On those occasions, you can write try! before the expression to disable error propagation and wrap the call in a runtime assertion that no error will be thrown. If an error actually is thrown, you’ll get a runtime error.
+        let _ = try! noErrorThrowsFunc()
+        
+        // (3) Specifying Cleanup Actions
+        //  This statement lets you do any necessary cleanup that should be performed regardless of how execution leaves the current block of code—whether it leaves because an error was thrown or because of a statement such as return or break.
+        defer {
+            print("End of Chapter 18.")
+        }
+        print("Hello, Defer.")
     }
+    
+    static func canThrowErrors() throws {
+        throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+    }
+    
+    static func anotherThrowsFunc() throws -> Int {
+        if arc4random() % 2 == 0 {
+            throw VendingMachineError.invalidSelection
+        }else {
+            return 11
+        }
+    }
+    
+    static func noErrorThrowsFunc() throws {
+        print("I never throw an error.")
+    }
+}
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
 }
--- Swift4CommandLine/Chapter19TypeCasting.swift
+++ Swift4CommandLine/Chapter19TypeCasting.swift
@@ -16,5 +16,141 @@ class Chapter19TypeCasting: NSObject {
         // (3) Downcasting
         // (4) Type Casting for Any and AnyObject
         
+        // Type Casting
+        // (1) Defining a Class Hierarchy for Type Casting
+        let library = [
+            MovieCp19(name: "Casablanca", director: "Michael Curtiz"),
+            SongCp19(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+            MovieCp19(name: "Citizen Kane", director: "Orson Welles"),
+            SongCp19(name: "The One And Only", artist: "Chesney Hawkes"),
+            SongCp19(name: "Never Gonna Give You Up", artist: "Rick Astley")
+        ]
+        // The items stored in library are still Movie and Song instances behind the scenes. However, if you iterate over the contents of this array, the items you receive back are typed as MediaItem, and not as Movie or Song. In order to work with them as their native type, you need to check their type, or downcast them to a different type, as described below.
+        
+        // (2) Checking Type
+        // is
+        var movieCount = 0, songCount = 0
+        for item in library {
+            if item is MovieCp19 {
+                movieCount += 1
+            }else if item is SongCp19 {
+                songCount += 1
+            }
+        }
+        
+        // (3) Downcasting
+        // The conditional form, as?, returns an optional value of the type you are trying to downcast to. This form of the operator will always return an optional value, and the value will be nil if the downcast was not possible.
+        // The forced form, as!, attempts the downcast and force-unwraps the result as a single compound action. This form of the operator will trigger a runtime error if you try to downcast to an incorrect class type.
+        for item in library {
+            // Try to access item as a Movie. If this is successful, set a new temporary constant called movie to the value stored in the returned optional Movie.
+            if let movie = item as? MovieCp19 {
+                print("Movie: \(movie.name), dir. \(movie.director)")
+            } else if let song = item as? SongCp19 {
+                print("Song: \(song.name), by \(song.artist)")
+            }
+        }
+        // Note:
+        /*
+         as
+         （1）从派生类转换为基类，向上转型（upcasts）
+         class Animal {}
+         class Cat: Animal {}
+         let cat = Cat()
+         let animal = cat as Animal
+         （2）消除二义性，数值类型转换
+         
+         let num1 = 42 as CGFloat
+         let num2 = 42 as Int
+         let num3 = 42.5 as Int
+         let num4 = (42 / 2) as Double
+         （3）switch 语句中进行模式匹配
+         如果不知道一个对象是什么类型，你可以通过switch语法检测它的类型，并且尝试在不同的情况下使用对应的类型进行相应的处理。
+         
+         switch animal {
+         case let cat as Cat:
+         print("如果是Cat类型对象，则做相应处理")
+         case let dog as Dog:
+         print("如果是Dog类型对象，则做相应处理")
+         default: break
+         }
+         */
+        
+        // (4) Type Casting for Any and AnyObject
+        // Swift provides two special types for working with nonspecific types:
+        // >>1 Any can represent an instance of any type at all, including function types.
+        // >>2 AnyObject can represent an instance of any class type.
+        
+        // Use Any and AnyObject only when you explicitly need the behavior and capabilities they provide. It is always better to be specific about the types you expect to work with in your code.
+        var things = [Any]()
+        
+        things.append(0)
+        things.append(0.0)
+        things.append(42)
+        things.append(3.14159)
+        things.append("hello")
+        // Tuple
+        things.append((3.0, 5.0))
+        // Object
+        things.append(MovieCp19(name: "Ghostbusters", director: "Ivan Reitman"))
+        // Closure
+        things.append({ (name: String) -> String in "Hello, \(name)" })
+        
+        for thing in things {
+            switch thing {
+            case 0 as Int:
+                print("zero as an Int")
+            case 0 as Double:
+                print("zero as a Double")
+            case let someInt as Int:
+                print("an integer value of \(someInt)")
+            case let someDouble as Double where someDouble > 0:
+                print("a positive double value of \(someDouble)")
+            case is Double:
+                print("some other double value that I don't want to print")
+            case let someString as String:
+                print("a string value of \"\(someString)\"")
+            case let (x, y) as (Double, Double):
+                print("an (x, y) point at \(x), \(y)")
+            case let movie as MovieCp19:
+                print("a movie called \(movie.name), dir. \(movie.director)")
+            case let stringConverter as (String) -> String:
+                print(stringConverter("Michael"))
+            default:
+                print("something else")
+            }
+        }
+        
+        
+        // Summary:
+        // 1. instance is Song, returns true or false
+        // 2. instance as? Song, returns optional Song, or "Song?"
+        //    instance as! Song, returns Song or casue runtime error
     }
 }
+
+class MediaCp19 {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class MovieCp19: MediaCp19 {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class SongCp19: MediaCp19 {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+
+
+
