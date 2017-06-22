77958ecf9483c9888eb1381bb5b74860901cf3db
naibin.liu <naibin.liu@witown.cn>
2017-06-22 18:26:56 +0800

    off work

--- Swift4CommandLine/Chapter22Protocols.swift
+++ Swift4CommandLine/Chapter22Protocols.swift
@@ -27,5 +27,38 @@ class Chapter22Protocols: NSObject {
         // (14) Optional Protocol Requirements
         // (15) Protocol Extensions
         
+        // Protocols
+        // A protocol defines a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
+        
+        // (1) Protocol Syntax
+        /*
+        protocol SomeProtocol {
+            
+        }
+        
+        struct SomeStructure: SomeProtocol {
+            
+        }
+        
+        class SomeClass: SomeSuperClass, SomeProtocol, AnotherProtocol {
+            
+        }
+         */
+        
+        // (2) Property Requirements
+        // (3) Method Requirements
+        // (4) Mutating Method Requirements
+        // (5) Initializer Requirements
+        // (6) Protocols as Types
+        // (7) Delegation
+        // (8) Adding Protocol Conformance with an Extension
+        // (9) Collections of Protocol Types
+        // (10) Protocol Inheritance
+        // (11) Class-Only Protocols
+        // (12) Protocol Composition
+        // (13) Checking for Protocol Conformance
+        // (14) Optional Protocol Requirements
+        // (15) Protocol Extensions
+        
     }
 }
